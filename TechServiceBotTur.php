8a75c43bc370abee7b63802ebc23c660fe1f8525
Alexey Yurasov <13106906+format37@users.noreply.github.com>
2020-09-25 08:42:59 +0300

    Update TechServiceBotTur.php

--- TechServiceBotTur.php
+++ TechServiceBotTur.php
@@ -15,34 +15,11 @@ function prepareStringForReturn($value)
 	return $value;
 }
 
-function saveToLog($value)
-{
-	$host='localhost';
-	$database='u440813_ice';
-	$user='root';
-	$pswd='';
-	$dbh = mysql_connect($host, $user, $pswd) or die("can't connect MySQL.");
-	mysql_select_db($database) or die("can't connect base.");
-	if (substr($value,0,2)=='in')
-	{
-		$state	= "0";
-		$username	= substr($value,3-strlen($value));
-	}
-	else
-	{
-		$state="1";
-		$username	= substr($value,4-strlen($value));
-	}
-
-	$query = "INSERT INTO `u440813_ice`.`iceTurEvents` (`EventDate`, `State`, `UserName`) VALUES ('".date("Y-m-d H:i:s",time())."','".$state."','".$username."')";
-	$res = mysql_query($query);
-}
 if (isset($_GET['name'])) {
 	//
 	$chat		= '-156425806';
 	$token	= '113484594:AAHePBzvcBtET8nJfiJCnCYVI6qmRY2nkTc';
 	$value	= $_GET['name'];
-    //saveToLog($value);//DISABLED
 	file_get_contents( 'https://api.telegram.org/bot'.$token.'/sendMessage?chat_id='.$chat.'&text='.$value );
 
 	//event history group
@@ -66,10 +43,11 @@ stripos($value, "maksim") === false &&
 stripos($value, "lex") === false &&
 stripos($value, "Vladimir_Polukhin") === false &&
 stripos($value, "Oleg_Semenov") === false &&
-stripos($value, "in_VictorB") === false
+stripos($value, "Anna_1c") === false &&
+stripos($value, "Ivan_T") === false &&
+stripos($value, "VictorB") === false
 ) ;
 else file_get_contents( 'https://api.telegram.org/bot'.$token.'/sendMessage?chat_id='.$chat.'&text=tur_'.$value );
-
 //1cGate--
 }
 ?>
